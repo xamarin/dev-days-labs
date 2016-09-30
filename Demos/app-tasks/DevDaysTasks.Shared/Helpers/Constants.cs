5370299153fcefaee89d93384fc73ebfc5a11628
James Montemagno <james.montemagno@gmail.com>
2016-09-30 14:13:05 -0700

    Update Nugets for Tasks app

--- Demos/app-tasks/DevDaysTasks.Shared/Helpers/Constants.cs
+++ Demos/app-tasks/DevDaysTasks.Shared/Helpers/Constants.cs
@@ -6,7 +6,7 @@ namespace DevDaysTasks
 	{
 		// Replace strings with your mobile services and gateway URLs.
 		public static string ApplicationURL =
-            @"https://motzcoffee.azurewebsites.net";
+            @"https://YOUR-APP-URL.azurewebsites.net";
 	}
 }
 
