384e8e09c38d806858c868f7384f2bb469eee67e
Geert van der Cruijsen <geert@vdcruijsen.net>
2016-11-08 21:55:09 +0100

    set x64 as default to not show message that app is slowing iOS down

--- Demos/app-imagesearch-cogs/ImageSearch/ImageSearch/Services/ServiceKeys.cs
+++ Demos/app-imagesearch-cogs/ImageSearch/ImageSearch/Services/ServiceKeys.cs
@@ -9,7 +9,7 @@ namespace ImageSearch.Services
     public class CognitiveServicesKeys
     {
 		//Setup a Bing Search API key from: http://www.microsoft.com/cognitive-services
-		public const string BingSearch = "f40aa3da36b2409ba0e2c41cf420a9b6";
+		public const string BingSearch = "f06bce968421495ab0286de6f83b8035";
 
 	} 
 }
