ce494e381416e023787c4adcbe54039a205bab43
James Montemagno <james.montemagno@gmail.com>
2016-11-18 14:15:06 -0800

    Update to latest NuGets and Keys

--- Demos/app-imagesearch-cogs/ImageSearch/ImageSearch/Services/ServiceKeys.cs
+++ Demos/app-imagesearch-cogs/ImageSearch/ImageSearch/Services/ServiceKeys.cs
@@ -9,7 +9,7 @@ namespace ImageSearch.Services
     public class CognitiveServicesKeys
     {
 		//Setup a Bing Search API key from: http://www.microsoft.com/cognitive-services
-		public const string BingSearch = "f40aa3da36b2409ba0e2c41cf420a9b6";
+		public const string BingSearch = "ef96c46d582f49aa8f0bb3c610573ce0";
 
 	} 
 }
