c86f3929d16d2d0c957e5b6c2c0bc4ec13601a28
Brandon Minnick <demo@Brandons-MacBook-Pro.local>
2018-06-13 15:51:36 +0200

    Added Missing Namespaces

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 using DevDaysSpeakers.Model;
 using DevDaysSpeakers.Services;
+
+using Xamarin.Forms;
 
 namespace DevDaysSpeakers.ViewModel
 {
