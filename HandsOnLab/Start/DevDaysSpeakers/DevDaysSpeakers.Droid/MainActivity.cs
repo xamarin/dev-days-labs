227d447dcb0d4b24a04e3c904ed728b44fa0efb6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-10 15:08:21 -0700

    Converted to PackageReference

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Droid/MainActivity.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Droid/MainActivity.cs
@@ -1,25 +1,29 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace DevDaysSpeakers.Droid
 {
     [Activity(Label = "Dev Days Speakers", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
     }
