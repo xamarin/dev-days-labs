9facbbd1a6ce15c7b6df9afd50bd2f7188802605
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-11 10:12:25 -0700

    Refactored AppDelegate & Main
    Added C#6 features like `nameof` and expression-bodied methods

--- HandsOnLab/Finish/DevDaysSpeakers/DevDaysSpeakers.iOS/AppDelegate.cs
+++ HandsOnLab/Finish/DevDaysSpeakers/DevDaysSpeakers.iOS/AppDelegate.cs
@@ -6,7 +6,7 @@ namespace DevDaysSpeakers.iOS
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
-    [Register("AppDelegate")]
+    [Register(nameof(AppDelegate))]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         //
