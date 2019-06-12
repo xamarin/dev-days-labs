018324d0cef8d20e30aa1dd02fd63062c6462535
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-11 18:46:42 -0700

    Updated UWP

--- HandsOnLab/Start/Src/DevDaysSpeakers.UWP/App.xaml.cs
+++ HandsOnLab/Start/Src/DevDaysSpeakers.UWP/App.xaml.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Reflection;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace DevDaysSpeakers.UWP
 {
-	/// <summary>
-	/// Provides application-specific behavior to supplement the default Application class.
-	/// </summary>
-	sealed partial class App : Application
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
 	{
 		/// <summary>
 		/// Initializes the singleton application object.  This is the first line of authored code
@@ -60,11 +52,11 @@ namespace DevDaysSpeakers.UWP
 
 				var assembliesToInclude = new List<Assembly>
                 {
-                	typeof(CachedImage).GetTypeInfo().Assembly,
+                	typeof(FFImageLoading.Forms.CachedImage).GetTypeInfo().Assembly,
                 	typeof(FFImageLoading.Forms.Platform.CachedImageRenderer).GetTypeInfo().Assembly
                 };
 
-				FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageSourceHandler()
+                FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
 				Xamarin.Forms.Forms.Init(e, assembliesToInclude);
 
 				if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
