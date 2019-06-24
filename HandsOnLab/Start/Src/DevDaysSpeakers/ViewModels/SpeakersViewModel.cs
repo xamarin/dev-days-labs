98a2e57e4de75dd652f985c9dc44ba68b84278cf
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-24 23:34:57 +0200

    Update SpeakersViewModel.cs

--- HandsOnLab/Start/Src/DevDaysSpeakers/ViewModels/SpeakersViewModel.cs
+++ HandsOnLab/Start/Src/DevDaysSpeakers/ViewModels/SpeakersViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 using DevDaysSpeakers.Shared.Models;
 using DevDaysSpeakers.Services;
+
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 
