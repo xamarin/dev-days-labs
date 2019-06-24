548e3c8631746b95d5ba19b3ba5220f2d655978a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-24 23:28:30 +0200

    Update SpeakersViewModel.cs

--- HandsOnLab/Start/Src/DevDaysSpeakers/ViewModels/SpeakersViewModel.cs
+++ HandsOnLab/Start/Src/DevDaysSpeakers/ViewModels/SpeakersViewModel.cs
@@ -12,60 +12,8 @@ using Xamarin.Forms;
 
 namespace DevDaysSpeakers.ViewModel
 {
-    public class SpeakersViewModel : INotifyPropertyChanged
+    public class SpeakersViewModel
     {
-        bool isBusy;
 
-        public SpeakersViewModel()
-        {
-            GetSpeakersCommand = new Command(async () => await GetSpeakers(), () => !IsBusy);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public Command GetSpeakersCommand { get; }
-
-        public ObservableCollection<Speaker> Speakers { get; } = new ObservableCollection<Speaker>();
-
-        public bool IsBusy
-        {
-            get => isBusy;
-            set
-            {
-                isBusy = value;
-                OnPropertyChanged();
-
-                GetSpeakersCommand.ChangeCanExecute();
-            }
-        }
-
-        async Task GetSpeakers()
-        {
-            try
-            {
-                IsBusy = true;
-
-                var items = await AzureService.GetSpeakers();
-
-                Speakers.Clear();
-
-                foreach (var item in items)
-                {
-                    Speakers.Add(item);
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Error: " + e.Message);
-                await Application.Current.MainPage.DisplayAlert("Error!", e.Message, "OK");
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-
-        void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
